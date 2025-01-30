0cbaa4013a034e412aa23b31a50323243e8e2cd8
apateltrueigtech <apateltrueigtech.com>
2025-01-30 14:13:52 +0530

    structure

--- React-app/src/Component/Home.jsx
+++ React-app/src/Component/Home.jsx
@@ -4,17 +4,106 @@ const Home = () => {
   return (
     <>
 <div className="min-h-screen bg-gray-700 text-gray-100 flex items-center justify-center">
-    <h1 className="text-4xl font-bold flex-gap-2.5">Dive Into Endless Fun with Social Casino Games Today</h1>
-    <p className='text-gray-500'>Embark on an unforgettable adventure where excitement knows no limits! With a vast
+    <div>
+    <h1 className="text-4xl font-bold flex-gap-3.5">Dive Into Endless Fun with Social Casino Games Today</h1>
+    <p className='text-light to-blue-400'>Embark on an unforgettable adventure where excitement knows no limits! With a vast
      of social games at your fingertips, you can experience the thrill of
     spinning the reels, testing your luck at the tables, and much more—all completely free.
-    Whether you're a seasoned player or new to the world of social casino games, there’s
+    Whether you are a seasoned player or new to the world of video games, there
     something here for everyone. Join us today and dive into a world of fun, excitement,
     and endless possibilities. Your ultimate gaming journey starts now—play for the joy of it!</p>
-
-    <button className="w-40 h-12 bg-blue-500 text-white font-semibold text-center rounded-lg shadow-lg hover:bg-blue-600 focus:ring-2 focus:ring-blue-300">
+    </div>
+    <div>
+    <button className="w-50 h-20 bg-blue-500 text-white font-semibold text-center rounded-lg shadow-lg hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 text-2xl" >
     Start Playing
     </button>
+    <img src="https://socialgameshouse.com/assets/Image/about.png"/>
+    <div>
+    <h2 className="text-4xl font-bold flex-gap-3.5">All Our Featured Games</h2>
+    <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+    <div className="relative rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+    <img src="https://socialgameshouse.com/assets/Image/Image%202.png" alt="Frozen Gems" className="w-full h-80 object-cover"/>
+    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black-50 p-6 flex flex-col justify-end">
+    <h3 className="text-white font-bold text-xl">FROZEN GEMS</h3>
+    </div>
+    </div>
+    </div>
+    <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+     <div className="relative rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+    <img src="https://socialgameshouse.com/assets/Image/Image%204.png" alt="Frozen Gems" className="w-full h-80 object-cover"/>
+    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black-50 p-6 flex flex-col justify-end">
+    <h3 className="text-white font-bold text-xl">AGENT OF HEARTS</h3>
+    </div>
+    </div>
+  </div>
+  <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+  <div className="relative rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+    <img src="https://socialgameshouse.com/assets/Image/Image%205.png" alt="Frozen Gems" className="w-full h-80 object-cover"/>
+    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black-50 p-6 flex flex-col justify-end">
+      <h3 className="text-white font-bold text-xl">SWEET ALCHEMY</h3>
+      </div>
+    </div>
+  </div>
+  <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+  <div className="relative rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+    <img src="https://socialgameshouse.com/assets/Image/Image%206.png" alt="Frozen Gems" className="w-full h-80 object-cover"/>
+    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black-50 p-6 flex flex-col justify-end">
+      <h3 className="text-white font-bold text-xl">CLASH OF CHAMELOT</h3>
+      </div>
+    </div>
+  </div>
+  <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+  <div className="relative rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+    <img src="https://socialgameshouse.com/assets/Image/Image%203.png" alt="Frozen Gems" className="w-full h-80 object-cover"/>
+    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black-50 p-6 flex flex-col justify-end">
+      <h3 className="text-white font-bold text-xl">CHAMPIONS OF MITHRUNE</h3>
+      </div>
+    </div>
+  </div>
+  <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+  <div className="relative rounded-lg overflow-hidden shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+    <img src="https://socialgameshouse.com/assets/Image/Image%201.png" alt="Frozen Gems" className="w-full h-80 object-cover"/>
+    <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black-50 p-6 flex flex-col justify-end">
+      <h3 className="text-white font-bold text-xl">BULL IN CHINA SHOP</h3>
+      </div>
+    </div>
+  </div>
+
+
+
+    </div>
+    </div>
+    <h2 className="text-4xl font-bold flex-gap-3.5">Why Choose Us ?</h2>
+    <div className="max-w-sm mx-auto bg-yellow border border-gray-200 rounded-lg shadow-lg     flex items-center justify-between">
+    <div className="p-4 ">
+    <h5 className="text-xl font-bold text-gray-800 mb-2">Endless Free Fun</h5>
+    <p className="text-gray-600 text-sm mb-4">
+    Enjoy a vast selection of social casino games—absolutely free! Play as much as you want, whenever you want, with no strings attached.
+    </p>
+    </div>
+    </div>
+    <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-lg">
+    <div className="p-4">
+    <h5 className="text-xl font-bold text-gray-800 mb-2">Wide Variety of Games</h5>
+    <p className="text-gray-600 text-sm mb-4">
+    We offer a diverse collection of games to suit every player’s taste. From novice to expert, there’s something for everyone to enjoy!
+    </p>
+    </div>
+    </div>
+    <div className="max-w-sm mx-auto bg-white border border-gray-200 rounded-lg shadow-lg">
+    <div className="p-4">
+    <h5 className="text-xl font-bold text-gray-800 mb-2">Play Anytime, Anywhere</h5>
+    <p className="text-gray-600 text-sm mb-4">
+    Our platform is designed for easy access, allowing you to play on any device. Wherever you are, you can dive into the fun whenever the mood strikes.
+    </p>
+    </div>
+    </div>
+    <div>
+    <h2 className="text-4xl font-bold flex-gap-3.5">Disclaimer</h2>
+    <p>Our platform is designed solely for entertainment, with no real money rewards. All games are completely free to play, and we are not responsible for any content. If you encounter inappropriate material, please contact us immediately.
+    We prioritize creating a safe, enjoyable experience and promote responsible play. Please note, participation on our platform has no impact on future real money gaming or winnings. Users must be 18 or older to participate.</p>
+    <img src="https://socialgameshouse.com/assets/Image/Disclaimer.png"/>
+</div>
 </div>
     </>    
   )
